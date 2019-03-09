9ee42e796e4883b9c272f6ffda200a6e9f032563
Subrat <subratpalhar92@gmail.com>
2019-03-09 15:43:58 +0530

    Google calender api

---
+++ settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Google App Client Id */
+define('CLIENT_ID', '666958696073-2lcmle4if9vektm4ot5si6ne6lueipq4.apps.googleusercontent.com');
+
+/* Google App Client Secret */
+define('CLIENT_SECRET', 'xnFYBkEHWNe4XMrB0PDbj9YK');
+
+/* Google App Redirect Url */
+define('CLIENT_REDIRECT_URL', 'http://localhost/mycalender/google-login.php');
+//http://localhost:/google-login.php
+?>
